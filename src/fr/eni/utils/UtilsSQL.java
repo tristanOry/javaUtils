96c437e2a1a7aac0531c452e2508f183bb315f2a
tristanOry <t.ory@laposte.net>
2023-02-28 14:38:01 +0100

    Commentaires

--- src/fr/eni/utils/UtilsSQL.java
+++ src/fr/eni/utils/UtilsSQL.java
@@ -7,10 +7,20 @@ public class UtilsSQL {
 	
 	public static final int EPOCH_DAY_TO_MS = 24 * 3600 * 1000; 
 	
+	/**
+	 * 
+	 * @param date une sql.date
+	 * @return une LocalDate
+	 */
 	public static LocalDate sqlDateToLocal (Date date) {
 		return LocalDate.ofEpochDay(date.getTime()/EPOCH_DAY_TO_MS);
 	}
 	
+	/**
+	 * 
+	 * @param date une LocalDate
+	 * @return une sql.date
+	 */
 	public static Date localDateToSql(LocalDate date) {
 		return new Date(date.toEpochDay() * EPOCH_DAY_TO_MS);
 	}
